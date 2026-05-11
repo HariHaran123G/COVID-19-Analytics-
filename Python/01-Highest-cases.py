5ec718daccd4f4a539091f4112c47a111c053bc5
HariHaran123G <hariharan3141592@gmail.com>
2026-05-11 16:59:15 +0530

    Add script to analyze and visualize COVID-19 cases
    This script loads a COVID-19 dataset, groups confirmed cases by country, and visualizes the top 10 countries with the highest confirmed cases using a bar chart.

---
+++ Python/01-Highest-cases.py
@@ -0,0 +1,24 @@
+#import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load dataset
+df=pd.read_excel('Data/Covid_19_Analytics_Dataset.xlsx')
+
+print(df.head())
+print(df.columns)
+country_cases=df.groupby('country')['confirmed_cases'].sum()
+country_cases=country_cases.reset_index()
+highest=country_cases.sort_values(by='confirmed_cases',ascending=False)
+print(highest.head(10))
+
+#matplotlib
+top10=highest.head(10)
+plt.bar(top10['country'],top10['confirmed_cases'])
+plt.title('Top 10 countries confirmed covid cases')
+plt.xlabel('country')
+plt.ylabel('confirmed_cases')
+plt.xticks(rotation=45)
+plt.show()
+
