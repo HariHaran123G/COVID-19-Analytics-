c56e768e3b7671249c001400c6001e70c530e284
HariHaran123G <hariharan3141592@gmail.com>
2026-05-12 11:43:09 +0530

    Fix import statements for numpy and matplotlib

--- Scripts/03.py
+++ Scripts/03.py
@@ -1,7 +1,7 @@
 #fatality rate analysis
 import pandas as pd
-import numpy as ny
-import matplotlib as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
 df=pd.read_excel('Data/Covid_19_Analytics_Dataset.xlsx')
 country_data=df.groupby('country').[['deaths','confirmed_cases']].sum()
