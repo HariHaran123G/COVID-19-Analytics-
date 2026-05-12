005d6724c3e92e82ddf887296875a1c07ec63fea
HariHaran123G <hariharan3141592@gmail.com>
2026-05-12 11:38:22 +0530

    Add fatality rate calculation and bar chart for top 10
    Calculate fatality rate and display top 10 countries.

--- Scripts/03.py
+++ Scripts/03.py
@@ -4,5 +4,18 @@ import numpy as ny
 import matplotlib as plt
 
 df=pd.read_excel('Data/Covid_19_Analytics_Dataset.xlsx')
-country_data=df.groupby('country').['deaths','confirmed_cases'].sum()
+country_data=df.groupby('country').[['deaths','confirmed_cases']].sum()
 country_data=country_data.reset_index()
+country_data['fatality_rate']=(country_data['deaths']/country_data['confirmed_cases'])*100
+highest_fatality=country_data.sort_values(by='fatality_rate',ascending=false)
+top10=highest_fatality.head(10)
+print(top10)
+
+#chart
+plt.figure(figsize=(12,6))
+plt.bar(top10['country'],top10['fatality_rate'])
+plt.title('Top 10 countries COVID Fatality rate')
+plt.xlabel('Country')
+plt.ylabel('Fatality rate(%)')
+plt.xticks(rotation=45)
+plt.show()
