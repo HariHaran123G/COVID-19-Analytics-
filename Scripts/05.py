ae7866b0c484d39e46da9ad3da4a66638aee6a2d
HariHaran123G <hariharan3141592@gmail.com>
2026-05-12 17:06:08 +0530

    Calculate recovery and death rates for countries
    Added recovery and death rate calculations to country data.

--- Scripts/05.py
+++ Scripts/05.py
@@ -4,4 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df=pd.read_excel('Data/Covid_19_Analytics_Dataset.xlsx')
-country_data=df.groupby('country')['confirmed_cases','recovered','deaths'].sum()
+country_data=df.groupby('country')[['confirmed_cases','recovered','deaths']].sum()
+country_data=country_data.reset_index()
+country_data['recovery_rate']=(country_data['recovered']/country_data['confirmed_cases'])*100
+country_data['death_rate']=(country_data['deaths']/country_data['confirmed_cases'])*100
+print(country_data.head())
+plt.figure(figsize=(10,6))
+plt.scatter(country_data['recovery_rate'],country_data['death_rate'])
+plt.title('Recovery rate vs death rate')
+plt.xlabel('Recovery rate %')
+plt.ylabel('Death rate %')
+plt.show()
